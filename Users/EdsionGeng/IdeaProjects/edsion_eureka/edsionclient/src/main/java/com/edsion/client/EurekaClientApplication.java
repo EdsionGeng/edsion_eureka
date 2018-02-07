86e50565ec38b57168238f3f95b8fe465d178cab
EdsionGeng <841851827@qq.com>
2018-02-07 15:00:37 +0800

    erueka搭建

---
+++ Users/EdsionGeng/IdeaProjects/edsion_eureka/edsionclient/src/main/java/com/edsion/client/EurekaClientApplication.java
@@ -0,0 +1,30 @@
+package com.edsion.client;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by EdsionGeng on 2018/2/7.
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class EurekaClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class, args);
+    }
+
+    @Value("${server.port}")
+    String port;
+
+    @RequestMapping("/hi")
+    public String home(@RequestParam String name) {
+        return "你好" + name + ",服务请求于端口：" + port;
+    }
+
+}
---
+++ Users/EdsionGeng/IdeaProjects/edsion_eureka/edsionserver/src/main/java/com/wsd/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,16 @@
+package com.wsd.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by EdsionGeng on 2018/2/7.
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
